7cd54dd0043fc161e74cfecf26e1a5d47ff8656c
Glaucio Daniel <glauciodaniel@gmail.com>
2021-08-27 19:12:05 -0300

    Finalizando o projeto

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,18 @@
 "use strict";
 //menuhotdog tudo em minusculo vai dar erro se chamar de outra forma
 //
+
 let menuHotDog = document.querySelector("#menu-hotdog");
 let body = document.querySelector("body");
+
+const numero = () => {
+  return parseInt(Math.random() * (9 - 0) + 0);
+};
+const fb = document.querySelector(".full-banner");
+fb.style.backgroundImage = `url('assets/images/background/${numero()}.jpg')`;
+setInterval(() => {
+  fb.style.backgroundImage = `url('assets/images/background/${numero()}.jpg')`;
+}, 7000);
 
 //simulando um banco de dados
 
@@ -57,161 +67,143 @@ let database = [
   },
 
   {
-    id: "994382216866168123871",
+    id: "94298482573727342343241266457",
     categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    nome: "Cristo Redentor - Corcovado",
+    site: "https://www.tremdocorcovado.rio/ ",
+    endereco: "Parque Nacional da Tijuca- Alta da Boa Vista",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/94298482573727342343241266457.jpg",
     ingresso: {
-      inteira: 0.0,
-      meia: 0.0,
+      alta: 55.4,
+      baixa: 38.4,
+      criancas: 21.6,
+      idosos: 13.1,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "Cristo Redentor é uma estátua art déco que retrata Jesus Cristo, localizada no topo do morro do Corcovado, a 709 metros acima do nível do mar, no Parque Nacional da Tijuca, com vista para a maior parte da cidade do Rio de Janeiro, Brasil. Em 2007 foi eleito informalmente como uma das sete maravilhas do mundo moderno",
+    longitudeLatidude: "@-22.9519,-43.2105,18z",
   },
 
   {
-    id: "994382216866168123871",
+    id: "7654234557654323342",
     categoria: 3,
-    nome: "Praia de Copacabana",
+    nome: "Praia do secreto - Recreio dos Bandeirantes",
     site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    endereco: "Trilha saindo da Av. do Estado da Guanabara ",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/7654234557654323342.jpg",
     ingresso: {
       inteira: 0.0,
       meia: 0.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "Para chegar na Praia do Secreto é preciso fazer uma mini trilha que passa por uma parte de mato e depois chega-se a um paredão bem um pouco íngreme de uns 15 metros de altura. A entrada da trilha não é sinalizada e existem pelo menos dois pontos de início da trilha saindo da Av. do Estado da Guanabara. <br/> Logo após o Mirante da Prainha sentido Recreio já aparecem as trilhas. Recomento deixar o carro no Mirante e partir andando para a Praia do Secreto.",
+    longitudeLatidude: "@-23.0187,-43.4789z",
   },
 
   {
-    id: "994382216866168123871",
+    id: "234567654345678232",
     categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    nome: "Morro do pontal",
+    site: "https://www.vidasemparedes.com.br",
+    endereco: " próxima ao Posto 12 na praia do Recreio dos Bandeirantes",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/234567654345678232.jpg",
     ingresso: {
       inteira: 0.0,
       meia: 0.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "Famosa na voz de Tim Maia, ela oferece um visual incrível das praias e montanhas do Rio. E o melhor, é acessível, com trilha de pouco mais de 500 metros, e nem precisa chegar até o cume para ter uma vista bacana",
+    longitudeLatidude: "@-21.0121,-48.0214,18z",
   },
 
   {
-    id: "994382216866168123871",
-    categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    id: "987656786545678244324",
+    categoria: 5,
+    nome: "Feira de São Cristóvão",
+    site: "https://www.viagensecaminhos.com/ ",
+    endereco: "R. Campo de São Cristóvão - São Cristóvão",
     cidade: "Rio de Janeiro",
     estado: "RJ",
-    cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    cep: "20921-240",
+    urlImagem: "assets/images/gallery/987656786545678244324.jpg",
     ingresso: {
-      inteira: 0.0,
-      meia: 0.0,
+      inteira: 5.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "O centro Luiz Gonzaga de tradições nordestinas, apresenta o melhor da arte e gastronomia, é repleto de lojinhas com produtos do nordeste brasileiro",
+    longitudeLatidude: "@-22.9095,-43.182365,18z",
   },
 
   {
-    id: "994382216866168123871",
-    categoria: 3,
-    nome: "Praia de Copacabana",
+    id: "9877823782362334234523",
+    categoria: 4,
+    nome: "Maracanã",
     site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    endereco: "Av. Pres. Castelo Branco, Portão 3 - Maracanã ",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/9877823782362334234523.jpg",
     ingresso: {
-      inteira: 0.0,
-      meia: 0.0,
+      inteira: 60.0,
+      meia: 30.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "Estádio Jornalista Mário Filho, mais conhecido como Maracanã, ou carinhosamente como Maraca, é um estádio de futebol localizado na Zona Norte da cidade brasileira do Rio de Janeiro.",
+    longitudeLatidude: "@-22.544830,-43.134367,18z",
   },
 
   {
-    id: "994382216866168123871",
-    categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    id: "9876567243432586754843872498012",
+    categoria: 1,
+    nome: "Museu do Amanhã",
+    site: "https://www.museudoamanhã.org.br",
+    endereco: "Praça Mauá, 1 - Centro",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/9876567243432586754843872498012.jpg",
     ingresso: {
       inteira: 0.0,
       meia: 0.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "O Museu do Amanhã é um museu de ciências aplicadas que explora as oportunidades e os desafios que a humanidade terá de enfrentar nas próximas décadas a partir das perspectivas da sustentabilidade e da convivência. Inaugurado em dezembro de 2015 pela Prefeitura do Rio, é um equipamento cultural da Secretaria Municipal de Cultura, que opera sob gestão do Instituto de Desenvolvimento e Gestão  O Museu foi originalmente concebido pela Fundação Roberto Marinho",
+    longitudeLatidude: "@-22.8939,-43.1794,18z",
   },
 
   {
-    id: "994382216866168123871",
+    id: "72397643995290253892",
     categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
+    nome: "Bondinho do Pão de Açucar",
+    site: "https://www.bondinho.com.br/",
+    endereco: "Av. Pasteur, 520 - Urca",
     cidade: "Rio de Janeiro",
     estado: "RJ",
     cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
+    urlImagem: "assets/images/gallery/72397643995290253892.jpg",
     ingresso: {
-      inteira: 0.0,
+      inteira: 114.0,
       meia: 0.0,
     },
     textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
-  },
-
-  {
-    id: "994382216866168123871",
-    categoria: 3,
-    nome: "Praia de Copacabana",
-    site: "https://www.riotur.rio",
-    endereco: "Avenida Atlântica, Copacabana ",
-    cidade: "Rio de Janeiro",
-    estado: "RJ",
-    cep: "22070-000",
-    urlImagem: "assets/images/gallery/994382216866168123871.jpg",
-    ingresso: {
-      inteira: 0.0,
-      meia: 0.0,
-    },
-    textoCurto:
-      "A praia carioca reflete um pouco o do que é o Rio de Janeiro,  um local alto astral, com uma beleza natural ímpar. A praia é cumprida e tem uma longa faixa de areia, que durante o verão fica lotada de pessoas",
-    longitudeLatidude: "@-22.970722,-43.182365,18z",
+      "O bondinho do Pão de Açúcar foi construído por meio de uma ousada operação, que levou três anos (1909-1912) e envolveu cerca de 400 pessoas. Para construir a estrutura, alpinistas escalaram os morros da Urca e do Pão de Açúcar, com equipamentos em mochilas, e operários se arriscaram na edificação das estações a 400 m de altura.",
+    longitudeLatidude: "@-22.9492,-43.1545,18z",
   },
 ];
 //Criando um componente modo Raiz
 // REACT - React modo raiz.
 
-const cg = document.querySelector(".caroussel-gallery");
+const cg = document.querySelectorAll(".caroussel-gallery");
 
 galeria(database, cg, 0);
 
@@ -253,17 +245,21 @@ function bloco(cg, item, counter) {
   cg.appendChild(card);
 }
 function galeria(db, cg, filtro = 0) {
-  cg.innerHMTL = "";
-  let counter = -5;
-  db.forEach((item) => {
-    if (filtro > 0 && filtro == parseInt(item.categoria)) {
-      bloco(cg, item, counter);
-      console.log("com filtro!", filtro);
-    } else if (filtro == 0) {
-      bloco(cg, item, counter);
-      console.log("sem filtro!", cg);
-    }
-    counter++;
+  cg.forEach((cgEl) => {
+    cgEl.innerHMTL = "";
+
+    let counter = -5;
+    db.forEach((item) => {
+      if (filtro > 0 && filtro == parseInt(item.categoria)) {
+        bloco(cgEl, item, counter);
+        console.log("com filtro!", filtro);
+      } else if (filtro == 0) {
+        bloco(cgEl, item, counter);
+        console.log("sem filtro!", cg);
+      }
+
+      counter++;
+    });
   });
 }
 
